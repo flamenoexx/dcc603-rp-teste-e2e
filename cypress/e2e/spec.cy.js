68e332e8d45dbbb5a918c8fc23e41716b051ffd9
Lucas Werlang <lucaswewe2806@gmail.com>
2025-06-17 00:10:03 -0300

    Testes adicionados

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -68,4 +68,70 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+
+  it('Marca todas as tarefas como concluídas com o toggle-all', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+        .type('Estudar Cypress{enter}')
+        .type('Finalizar projeto de ES{enter}')
+        .type('Enviar relatório{enter}');
+
+    cy.get('[data-cy=toggle-all]')
+        .click();
+
+    cy.get('[data-cy=todos-list] > li')
+        .each((item) => {
+          cy.wrap(item).should('have.class', 'completed');
+        });
+  });
+
+  it('Edita o texto de uma tarefa já criada', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+        .type('Texto antigo{enter}');
+
+    cy.get('[data-cy=todos-list] li')
+        .first()
+        .find('label')
+        .dblclick();
+
+    cy.get('[data-cy=todos-list] li')
+        .first()
+        .find('.edit')
+        .clear()
+        .type('Texto atualizado{enter}');
+
+    cy.get('[data-cy=todos-list] li')
+        .first()
+        .should('contain.text', 'Texto atualizado');
+  });
+
+  it('Limpa somente as tarefas que estão concluídas', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+        .type('Tarefa completa 1{enter}')
+        .type('Tarefa ativa{enter}')
+        .type('Tarefa completa 2{enter}');
+
+    cy.get('[data-cy=todos-list] li')
+        .first()
+        .find('[data-cy=toggle-todo-checkbox]')
+        .click();
+
+    cy.get('[data-cy=todos-list] li')
+        .last()
+        .find('[data-cy=toggle-todo-checkbox]')
+        .click();
+
+    cy.get('.clear-completed')
+        .click();
+
+    cy.get('[data-cy=todos-list] li')
+        .should('have.length', 1)
+        .and('contain.text', 'Tarefa ativa');
+  });
+
 });
